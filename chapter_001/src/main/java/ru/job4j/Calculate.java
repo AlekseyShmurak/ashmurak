fdcac770b2f6a2966a6a4083dbf386aa0a495775
AlekseyShmurak <shmur5755@gmail.com>
2018-02-14 21:51:05 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+* Calculate
+* @author Aleksey Shmurak (shmur5755@gmail.com)
+*/
+
 public class Calculate{
+	/**
+	* Method main
+	* @param args
+	*/
+	
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
