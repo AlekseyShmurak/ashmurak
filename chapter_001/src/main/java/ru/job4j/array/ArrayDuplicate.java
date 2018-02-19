da57690a8fba54626de386dcedcad7c874d51531
AlekseyShmurak <shmur5755@gmail.com>
2018-02-19 20:51:26 +0300

    5.6. Удаление дубликатов в массиве.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -6,14 +6,14 @@ public class ArrayDuplicate {
 
     public String[] remove(String[] array) {
         int dublicates = 0;
-        for (int i=0; i < array.length-dublicates; i++) {
-            for (int j=i+1; j < array.length-dublicates; j++){
-                if (array[j].equals(array[i])){
-                    array[j] = array[array.length-1-dublicates];
+        for (int i = 0; i < array.length - dublicates; i++) {
+            for (int j = i + 1; j < array.length - dublicates; j++) {
+                if (array[j].equals(array[i])) {
+                    array[j] = array[array.length - 1 - dublicates];
                     j--;
                     dublicates++;
                 }
             }
-        }return Arrays.copyOf(array,array.length-dublicates);
+        } return Arrays.copyOf(array, array.length - dublicates);
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -10,8 +10,8 @@ public class ArrayDuplicateTest {
     @Test
     public void removeTest() {
         ArrayDuplicate test = new ArrayDuplicate();
-        String[] input = {"44","55","44","66","77","55","44","55"};
-        String[] expected = {"44","55","66","77"};
-        assertThat(expected,arrayContainingInAnyOrder(test.remove(input)));
+        String[] input = {"44", "55", "44", "66", "77", "55", "44", "55"};
+        String[] expected = {"44", "55", "66", "77"};
+        assertThat(expected, arrayContainingInAnyOrder(test.remove(input)));
     }
 }
