be6eb940d1f62f127a1f06996950fd52faff265e
AlekseyShmurak <shmur5755@gmail.com>
2018-03-01 03:38:22 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -37,7 +37,18 @@ public class StubInput implements Input {
 
     @Override
     public int ask(String question, int[] rage) {
-//        throw new UnsupportedOperationException("Unsupported Operation");
-        return -1;
+        int key = Integer.valueOf(ask(question));
+        boolean exist = false;
+        for (int value : rage) {
+            if(value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Ошибка") ;
+        }
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,16 +1,26 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private Input input;
 
-    public int ask( String question, int[] rage) {
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
+    public int ask(String question, int[] rage) {
         boolean invalid = true;
         int value = - 1;
         do{
             try {
-                value = super.ask(question, rage);
+                value = this.input.ask(question, rage);
                 invalid = false;
             } catch ( NumberFormatException error) {
-                System.out.println("Введены не верные данные");
+                System.out.println("Введены неверные данные");
             } catch ( MenuOutException moe) {
                 System.out.println("Введен не существующий пункт меню");
             }
