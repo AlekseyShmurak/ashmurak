4ee4efbcee1c1089e2d2d4a7f8057ec5794dd24a
AlekseyShmurak <shmur5755@gmail.com>
2018-03-06 01:03:51 +0300

    3. Конвертация двумерного массива в ArrayList и наоборот

---
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> toList (int[][] array) {
+        List<Integer> output = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (Integer integer : array[i]) {
+                output.add(integer);
+            }
+        }
+        return output;
+    }
+
+    public static int[][] toArray (List<Integer> list, int rows) {
+        int[][] output = new int[list.size() % rows == 0 ? list.size()/rows : list.size()/rows + 1][];
+        int listIndex = 0;
+        for (int i = 0; i < output.length; i++){
+            output[i] = new int[rows];
+            for (int j = 0; j < output[i].length; j++) {
+                if (listIndex != list.size()){
+                    output[i][j] = list.get(listIndex);
+                    listIndex++;
+                }
+            }
+        }
+        return output;
+    }
+}
+
+
---
+++ chapter_003/src/test/java/ru/job4j/search/ConvertListTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class ConvertListTest {
+
+    @Test
+    public void toListTest() {
+        int[][] input = {{1, 2, 3}, {3, 4, 5}};
+        List<Integer> output = ConvertList.toList(input);
+        Integer[] expect = {1, 2, 3, 3, 4, 5};
+        assertThat(expect, is(output.toArray()));
+    }
+
+    @Test
+    public void toArrayTest() {
+        int[][] expect = {{1, 2, 3}, {3, 4, 5}, {6, 0, 0}};
+        int[][] input = {{1, 2, 3, 3, 4, 5, 6}};
+
+        assertThat(expect, is(ConvertList.toArray(ConvertList.toList(input), 3)));
+    }
+
+}
