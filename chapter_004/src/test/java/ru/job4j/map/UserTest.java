1f11414b084e10bd67faba649ddd10180ba4d8d7
AlekseyShmurak <shmur5755@gmail.com>
2018-03-20 22:19:44 +0300

    2. Не перекрывать equals hashCode

---
+++ chapter_004/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    Calendar date = new GregorianCalendar();
+
+    Map<User, String> map = new HashMap();
+    User first = new User("Vasia", 2, date);
+    User second = new User("Vasia", 2, date);
+
+    @Test
+    public void mapTest() {
+        map.put(first, "asd");
+        map.put(second, "qwe");
+        Iterator<User> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().name);
+        }
+        assertThat(2, is(map.keySet().size()));
+    }
+}
