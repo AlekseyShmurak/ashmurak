38d21d07940766d403f03eca978c4519611412a7
AlekseyShmurak <shmur5755@gmail.com>
2018-03-23 21:07:55 +0300

    5.1.1. Итератор для двухмерного массива int[][]

--- chapter_004/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ chapter_004/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -4,8 +4,8 @@ import java.util.Iterator;
 
 public class MatrixIterator implements Iterator {
     private int[][] input;
-    private int externalArrayIndex = 0;
-    private int internalArrayIndex = 0;
+    private int columnInd = 0;
+    private int rowInd = 0;
 
 
     public MatrixIterator(int[][] input) {
@@ -14,18 +14,18 @@ public class MatrixIterator implements Iterator {
 
     @Override
     public boolean hasNext() {
-        return externalArrayIndex < input.length - 1
-                || (externalArrayIndex == input.length - 1 && internalArrayIndex < input[externalArrayIndex].length);
+        return columnInd < input.length - 1
+                || (columnInd == input.length - 1 && rowInd < input[columnInd].length);
     }
 
     @Override
     public Object next() {
         Object rslt;
-        if (input[externalArrayIndex].length > internalArrayIndex) {
-            rslt = (Integer) input[externalArrayIndex][internalArrayIndex++];
+        if (input[columnInd].length > rowInd) {
+            rslt = (Integer) input[columnInd][rowInd++];
         } else {
-            internalArrayIndex = 0;
-            rslt = input[++externalArrayIndex][internalArrayIndex++];
+            rowInd = 0;
+            rslt = input[++columnInd][rowInd++];
         }
         return rslt;
     }
