2ca3e6fc086285192aafa62014c7525efe8c5ddd
AlekseyShmurak <shmur5755@gmail.com>
2018-04-05 01:33:28 +0300

    2. Класс хранилища пользователей UserStorage

--- chapter_004/src/main/java/ru/job4j/generic/UserStore.java
+++ chapter_004/src/main/java/ru/job4j/generic/UserStore.java
@@ -7,18 +7,14 @@ public class UserStore extends AbstractStore<User> {
         this.values = new SimpleList<>(size);
     }
 
-    public boolean transfer(String fromId, String toId, int amount) {
+    public synchronized boolean transfer(String fromId, String toId, int amount) {
         boolean rslt = false;
-        int expect = findById(fromId).getAmount() - amount;
         User first = findById(fromId);
         User second = findById(toId);
         if (amount <= first.getAmount()) {
             first.setAmount(first.getAmount() - amount);
             second.setAmount(second.getAmount() + amount);
             rslt = true;
-        }
-        if (expect != first.getAmount()) {
-            rslt = transfer(fromId, toId, amount);
         }
         return rslt;
     }
