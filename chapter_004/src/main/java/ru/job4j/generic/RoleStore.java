cd62bb753e711488a0de5e7c8265b59b6525a90b
AlekseyShmurak <shmur5755@gmail.com>
2018-04-04 03:13:39 +0300

    Fix

--- chapter_004/src/main/java/ru/job4j/generic/RoleStore.java
+++ chapter_004/src/main/java/ru/job4j/generic/RoleStore.java
@@ -6,15 +6,4 @@ public class RoleStore extends AbstractStore<Role> {
         this.values = new SimpleList<Role>(size);
     }
 
-    @Override
-    public void add(Base model) {
-        if (model instanceof Role) {
-            super.add(model);
-        }
-    }
-
-    @Override
-    public boolean replace(String id, Base model) {
-        return  model instanceof Role && super.replace(id, model);
-    }
 }
