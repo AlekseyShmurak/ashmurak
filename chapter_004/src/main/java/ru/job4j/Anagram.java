2792468483a1ff5ae220034d0681575ddd2bb977
AlekseyShmurak <shmur5755@gmail.com>
2018-04-10 19:50:08 +0300

    Anagram

--- chapter_004/src/main/java/ru/job4j/Anagram.java
+++ chapter_004/src/main/java/ru/job4j/Anagram.java
@@ -1,10 +1,11 @@
 package ru.job4j;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Anagram {
 
-    public boolean isAnagram(String o1, String o2) {
+    public boolean isAnagramAr(String o1, String o2) {
         boolean rslt = false;
         char[] ar1 = o1.toCharArray();
         char[] ar2 = o2.toCharArray();
@@ -24,6 +25,31 @@ public class Anagram {
         char[] ar2 = o2.toCharArray();
         if (ar1.length == ar2.length && sum(ar1) == sum(ar2)) {
             rslt = true;
+        }
+        return rslt;
+    }
+
+    public boolean isAnagramHash(String o1, String o2) {
+        boolean rslt = true;
+        HashMap<Character, Integer> o1Table = new HashMap<>();
+        for (char ch : o1.toCharArray()) {
+            if (o1Table.containsKey(ch)) {
+                o1Table.replace(ch, o1Table.get(ch) + 1);
+            } else {
+                o1Table.put(ch, 1);
+            }
+        }
+        for (char ch : o2.toCharArray()) {
+            if (!o1Table.containsKey(ch)) {
+                rslt = false;
+                break;
+            } else {
+                if (o1Table.get(ch) == 1) {
+                    o1Table.remove(ch);
+                } else {
+                    o1Table.replace(ch, o1Table.get(ch) - 1);
+                }
+            }
         }
         return rslt;
     }
--- chapter_004/src/test/java/ru/job4j/AnagramTest.java
+++ chapter_004/src/test/java/ru/job4j/AnagramTest.java
@@ -5,20 +5,33 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnagramTest {
-    String str1 = "qwertasdfgq";
-    String str2 = "gfdsaqwertq";
-    Anagram anagram = new Anagram();
 
     @Test
-    public void isAnagramTrue() {
-        assertThat(true, is(anagram.isAnagram(str1, str2)));
-        assertThat(false, is(anagram.isAnagram(str1, "qweutasdfgq")));
+    public void isAnagramAr() {
+        String str1 = "qwertasdfgq";
+        String str2 = "gfdsaqwertq";
+        Anagram anagram = new Anagram();
+        assertThat(true, is(anagram.isAnagramAr(str1, str2)));
+        assertThat(false, is(anagram.isAnagramAr(str1, "qweutasdfgq")));
 
     }
 
     @Test
     public void isAnagramSum() {
+        String str1 = "qwertasdfgq";
+        String str2 = "gfdsaqwertq";
+        Anagram anagram = new Anagram();
         assertThat(true, is(anagram.isAnagramSum(str1, str2)));
         assertThat(false, is(anagram.isAnagramSum(str1, "qweutasdfgq")));
+    }
+
+    @Test
+    public void isAnagramHash() {
+        String str1 = "qwertasdfgq";
+        String str2 = "gfdsaqwertq";
+        Anagram anagram = new Anagram();
+        assertThat(true, is(anagram.isAnagramHash(str1, str2)));
+        assertThat(false, is(anagram.isAnagramHash(str1, "qweutasdfgq")));
+
     }
 }
