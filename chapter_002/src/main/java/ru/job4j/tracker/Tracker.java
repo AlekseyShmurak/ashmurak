94fe49e8bbe9509b2e8a8e44c9fa2b460138d54f
AlekseyShmurak <shmur5755@gmail.com>
2018-04-24 07:08:36 +0300

    Трекер

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -19,26 +19,15 @@ public class Tracker {
      * @return Item.
      */
     public Item add(Item item) {
-        Connection connection = null;
         item.setId(this.generateId());
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            PreparedStatement st = connection.prepareStatement("INSERT INTO items (id, name, description) VALUES (?,?,?)");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             PreparedStatement st = connection.prepareStatement("INSERT INTO items (id, name, description) VALUES (?,?,?)")) {
             st.setInt(1, item.getId());
             st.setString(2, item.getName());
             st.setString(3, item.getDesc());
             st.executeUpdate();
-            st.close();
             } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return item;
     }
@@ -48,25 +37,14 @@ public class Tracker {
      * @param item
      */
     public void replace(int id, Item item) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            PreparedStatement st = connection.prepareStatement("UPDATE items SET name = ?, description = ? WHERE id = ?");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             PreparedStatement st = connection.prepareStatement("UPDATE items SET name = ?, description = ? WHERE id = ?")) {
             st.setString(1, item.getName());
             st.setString(2, item.getDesc());
             st.setInt(3, id);
             st.executeUpdate();
-            st.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
     /**
@@ -74,23 +52,12 @@ public class Tracker {
      * @param id
      */
     public void delete(int id) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            PreparedStatement st = connection.prepareStatement("DELETE FROM items WHERE id = ?");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             PreparedStatement st = connection.prepareStatement("DELETE FROM items WHERE id = ?")) {
             st.setInt(1, id);
             st.executeUpdate();
-            st.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
     /**
@@ -98,28 +65,16 @@ public class Tracker {
      * @return
      */
     public List<Item> findAll() {
-        Connection connection = null;
         List<Item> output = new ArrayList<>();
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM items");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             Statement st = connection.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM items")) {
             while (rs.next()) {
                 output.add(new Item(rs.getInt("id"), rs.getString("name"),
                         rs.getString("description"), rs.getString("created")));
             }
-            rs.close();
-            st.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return output;
     }
@@ -129,29 +84,18 @@ public class Tracker {
      * @return
      */
     public List<Item> findByName(String key) {
-        Connection connection = null;
         List<Item> output = new ArrayList<>();
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            PreparedStatement st = connection.prepareStatement("SELECT * FROM items WHERE name = ?");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             PreparedStatement st = connection.prepareStatement("SELECT * FROM items WHERE name = ?")) {
             st.setString(1, key);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                output.add(new Item(rs.getInt("id"), rs.getString("name"),
-                        rs.getString("description"), rs.getString("created")));
-            }
-            rs.close();
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    output.add(new Item(rs.getInt("id"), rs.getString("name"),
+                            rs.getString("description"), rs.getString("created")));
                 }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return output;
     }
@@ -168,50 +112,28 @@ public class Tracker {
      * @return Item
      */
     public Item findById(int id) {
-        Connection connection = null;
         Item output = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            PreparedStatement st = connection.prepareStatement("SELECT * FROM items WHERE id = ?");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             PreparedStatement st = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
             st.setInt(1, id);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                output = new Item(rs.getInt("id"), rs.getString("name"),
-                        rs.getString("description"), rs.getString("created"));
-            }
-            rs.close();
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    output = new Item(rs.getInt("id"), rs.getString("name"),
+                            rs.getString("description"), rs.getString("created"));
                 }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return output;
     }
 
     public void deleteAllItems() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
-            Statement st = connection.createStatement();
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tracker", "SHMUR", "123");
+             Statement st = connection.createStatement()) {
             st.executeUpdate("DELETE FROM items");
-            st.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
